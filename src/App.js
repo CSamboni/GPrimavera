42fde2d96d114d1a7a33aa8849aa67cf7363a865
CSamboni <elementrixz512@gmail.com>
2018-08-01 19:15:49 -0500

    Inicio de la App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Encabezado from './Componentes/Encabezado';
+import Tarjetas from './Componentes/Tarjetas';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Encabezado />
+      <Tarjetas />
+
+      </div>
+    );
+  }
+}
+export default App;
