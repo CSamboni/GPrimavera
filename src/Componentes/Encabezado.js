785047129e5c6a7163ccb2d88eb49cfe3814b467
CSamboni <elementrixz512@gmail.com>
2018-08-01 20:25:30 -0500

    Modificado algunos assets

--- src/Componentes/Encabezado.js
+++ src/Componentes/Encabezado.js
@@ -3,20 +3,13 @@ import React, { Component } from 'react'
 class Encabezado extends Component {
     render() { 
         return (
-            <div>
             <section id="portada" className="hero is-info is-large">
         <div className="hero-body">
      <div className="container">
-      <h1 className="title">
-        Grupo Primavera
-      </h1>
-      <h2 className="subtitle">
-        Nos mueve la música
-      </h2>
     </div>
   </div>
 </section>
-            </div>
+
         );
     }
 }
